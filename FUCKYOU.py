b4a82bca414b557aef14a2cd2dcc78ed046f25c8
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-01 22:46:57 -0500

    Drawn middle finger

--- FUCKYOU.py
+++ FUCKYOU.py
@@ -4,6 +4,11 @@ from colorama import Fore, Style
 
 logo = f"""
 {Fore.YELLOW}THIS IS THE FUCK YOU VIRUS. FUCK YOU.{Fore.YELLOW}
+      ___
+      | |
+    __| |__
+   /|     |
+   \'-----'
 """
 
 print(logo)
