e0222aea86c4d3926835a3e6d31298b911781419
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-09 15:22:09 -0600

    Update FUCKYOU.py

--- FUCKYOU.py
+++ FUCKYOU.py
@@ -1,14 +1,15 @@
-import sys
+import sysimport winsound
 
 from colorama import Fore, Style
 
 logo = f"""
 {Fore.YELLOW}THIS IS THE FUCK YOU VIRUS. FUCK YOU.{Fore.YELLOW}
-      ___
-      | |
-    __| |__
-   /|     |
-   \'-----'
+     ___
+     | |
+    _| |__
+   /|   ' |
+   \|_____|
 """
 
 print(logo)
+winsound.PlaySound('sound.wav', winsound.SND_FILENAME) # figure out how to do this err
